f4cb3fc69b1556a2bd9e644dee3c4904476f3202
Florian Waltersdorfer <ignatiomobius@gmail.com>
2024-01-29 13:03:47 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    sprites.destroyAllSpritesOfKind(SpriteKind.Player)
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     spritePlayer.left = spritePlayer.left - spritePlayer.width
 })
